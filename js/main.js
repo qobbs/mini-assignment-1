412065acb10a0ce6073c9d54a70315f058fdd73b
Aaliyah Evans <amevans4@csuchico.edu>
2024-06-25 23:07:58 +0000

    I'm trying to create a button to generate random facts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $( document ).ready(function() {
-    $('#btn').click(function() {
+
+    let num = Math.random() * (3 + 1) + 1;
+
+    $('#fact button').click(function() {
+    
+    if(num = 1){
         window.alert("Did you know that sea butterflies are hermaphrodites, meaning they are both male and female?");
-    });
+    }
+
+    else if(num = 2){
+        window.alert("Though they seem exotic, they are very common and a vital resource for food and carbon consumption.");
+    }
+
+    else{
+    window.alert("They are known as 'the potato chip of the sea' because they are a vital sea source to many sea creatures.");
+    }
+    
+    });//end of click function
 });
